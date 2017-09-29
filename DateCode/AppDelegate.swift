3a3430dd9d30ae246925f3e4d30f31ed4ff37830
s.murakami <smurakami.84@gmail.com>
2017-09-29 17:50:49 +0900

    Initial Commit

---
+++ DateCode/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  DateCode
+//
+//  Created by 村上晋太郎 on 2017/09/29.
+//  Copyright © 2017年 村上晋太郎. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
